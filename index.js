c5ca3673d12cc8ef7aecbc0a82a90c2968f38a66
rishavchanda <rishavchanda0@gmail.com>
2022-11-16 23:09:29 +0530

    made change

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const connect = () => {
 };
 
 const corsOptions ={
-    origin: ['http://localhost:3000','https://meek-muffin-6f4e2b.netlify.app'], 
+    origin: 'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
     allowedHeaders: [
